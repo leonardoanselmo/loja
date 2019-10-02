19eec083f8c0803f5227c6665d1b7dcc4f4799fa
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2019-10-02 11:48:34 -0300

    Altera o produto na base de dados

--- altera-produto.php
+++ altera-produto.php
@@ -1,6 +1,5 @@
-<?php include("cabecalho.php");
-      include("conecta.php");
-      include("banco-produto.php");
+<?php require_once("cabecalho.php");      
+      require_once("banco-produto.php");
     
     $id = $_POST["id"];      
     $nome = $_POST["nome"];
@@ -22,4 +21,4 @@
         
 ?>
     
-<?php include("rodape.php");?>
+<?php require_once("rodape.php");?>
