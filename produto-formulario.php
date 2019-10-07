46c0ec987794c8173c917f7f9bf6aa985dac555a
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2019-10-07 09:32:10 -0300

    Formulário de produto.

--- produto-formulario.php
+++ produto-formulario.php
@@ -1,47 +1,20 @@
-<?php include("cabecalho.php");
-      include("conecta.php");
-      include("banco-categoria.php");
-      include("logica-usuario.php");
+<?php require_once("cabecalho.php");      
+      require_once("banco-categoria.php");
+      require_once("logica-usuario.php");
 
 verificaUsuario();
 
+$produto = array("nome" => "", "descricao" => "", "preco" => "", "categoria_id" => "1");
+$usado = "";
 $categorias = listaCategorias($conexao);
 ?>
     <h1>Formulário de produto</h1>
     <form action="adiciona-produto.php" method="post">
         <table class="table">
-            <tr>
-                <td>Nome:</td>
-                <td><input class="form-control" type="text" name="nome"></td>
-            </tr>
-            <tr>
-                <td>Preço:</td>
-                <td><input class="form-control" type="number" name="preco"></td>
-            </tr>
-            <tr>
-                <td>Descrição:</td>
-                <td><textarea class="form-control" name="descricao"></textarea></td>
-            </tr>    
-            <tr>
-                <td></td>
-                <td><input type="checkbox" name="usado" value="true"> Usado</td>
-
-            </tr>  
-            <tr>
-                <td>Categoria</td>
-                <td>
-                    <select name="categoria_id" class="form-control">
-                    <?php foreach($categorias as $categoria) : ?>
-                        <option value="<?=$categoria['id']?>">
-                                <?=$categoria['nome']?>
-                        </option>
-                    <?php endforeach ?>
-                    </select>                    
-                </td>
-            </tr>      
+            <?php require_once("produto-formulario-base.php"); ?>
             <tr>
                 <td><button class="btn btn-primary" type="submit">Cadastrar</button></td>
             </tr>
         </table>
     </form>
-<?php include("rodape.php");?>       
+<?php require_once("rodape.php");?>       
