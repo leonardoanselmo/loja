a9f886211a85461f3b7b83c8a3a003f60cbbb4d8
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2019-10-07 09:28:26 -0300

    Cabecalho da loja virtual.

--- cabecalho.php
+++ cabecalho.php
@@ -1,28 +1,39 @@
+<?php 
+    error_reporting(E_ALL ^ E_NOTICE);
+    require_once("mostra-alerta.php");     
+?>
+
 <html>
 <head>
     <meta charset="utf-8">
     <title>Minha Loja</title>
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/loja.css" rel="stylesheet">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/loja.css" rel="stylesheet">    
 </head>
-<body>
-
-    <div class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="index.php">Minha Loja</a>
-            </div>
-            <div>
-                <ul class="nav navbar-nav">
-                    <li><a href="produto-formulario.php">Adiciona produto</a></li>
-                    <li><a href="produto-lista.php">Produtos</a></li>
-                </ul>
-
-            </div>
-
+<body>   
+    
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php">Minha Loja</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="produto-formulario.php">Adiciona produtos <span class="sr-only">(Página atual)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="produto-lista.php">Produtos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contato.php">Contato</a>
+                </li>      
+                <li class="nav-item">
+                    <a class="nav-link" href="logout.php">Logout</a>
+                </li>      
+            </ul>
         </div>
-    </div>
+    </nav>
 
-
-    <div class="container">
+   <div class="container">
         <div class="principal">
